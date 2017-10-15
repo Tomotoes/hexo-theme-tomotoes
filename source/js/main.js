37343a46536328930d492219d3f91b4ef1f41384
Simon <jinmaup@gmail.com>
2017-10-15 19:27:22 +0800

    增加3D标题效果

--- source/js/main.js
+++ source/js/main.js
@@ -501,4 +501,35 @@
     } else {
         console.error('Waves loading failed.')
     }
+
+    /*3D文字*/
+    var text = document.getElementById('text'),
+    body = document.body,
+    steps = 7;
+    function threedee (e) {
+        var x = Math.round(steps / (window.innerWidth / 2) * (window.innerWidth / 2 - e.clientX)),
+            y = Math.round(steps / (window.innerHeight / 2) * (window.innerHeight / 2 - e.clientY)),
+            shadow = '',
+            color = 190,
+            radius = 3,
+            i;
+        
+        for (i=0; i<steps; i++) {
+            tx = Math.round(x / steps * i);
+            ty = Math.round(y / steps * i);
+            if (tx || ty) {
+                color -= 3 * i;
+                shadow += tx + 'px ' + ty + 'px 0 rgb(' + color + ', ' + color + ', ' + color + '), ';
+            }
+        }
+
+        shadow += x + 'px ' + y + 'px 1px rgba(0,0,0,.2), ' + x*2 + 'px ' + y*2 + 'px 6px rgba(0,0,0,.3)';
+        
+        text.style.textShadow = shadow;
+        text.style.webkitTransform = 'translateZ(0) rotateX(' + y*1.5 + 'deg) rotateY(' + -x*1.5 + 'deg)';
+        text.style.MozTransform = 'translateZ(0) rotateX(' + y*1.5 + 'deg) rotateY(' + -x*1.5 + 'deg)';
+    }
+    /*var 3dtext=document.querySelector(".content-header")*/
+    document.querySelectorAll("header")[1].addEventListener('mousemove', threedee, false);
+
 })(window, document);
